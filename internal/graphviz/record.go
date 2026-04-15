331db8dd9811540e734a3b20d9990bf57f57e5b0
Bevan Arps <bevan.arps@microsoft.com>
2026-04-15 23:13:47 +0000

    chore: adopt iotamixing, funcorder, forbidigo, embeddedstructfieldcheck linters

--- internal/graphviz/record.go
+++ internal/graphviz/record.go
@@ -17,6 +17,26 @@ func newRecord() *record {
 	return &record{}
 }
 
+var escapings = map[string]string{
+	`{`: `\{`,
+	`}`: `\}`,
+	`"`: `\"`,
+}
+
+// String returns the string representation of the record, which is the parts joined by " | ".
+func (r *record) String() string {
+	if len(r.parts) == 1 {
+		return r.parts[0]
+	}
+
+	content := strings.Join(r.parts, " | ")
+	for s, r := range escapings {
+		content = strings.ReplaceAll(content, s, r)
+	}
+
+	return fmt.Sprintf("{%s}", content)
+}
+
 // add adds a part to the record.
 func (r *record) add(text string) {
 	if len(text) > 0 {
@@ -38,24 +58,4 @@ func (r *record) addWrapped(width int, text string) {
 // addWrappedf adds a formatted part to the record, wrapping the text to the specified width.
 func (r *record) addWrappedf(width int, format string, args ...any) {
 	r.addWrapped(width, fmt.Sprintf(format, args...))
-}
-
-var escapings = map[string]string{
-	`{`: `\{`,
-	`}`: `\}`,
-	`"`: `\"`,
-}
-
-// String returns the string representation of the record, which is the parts joined by " | ".
-func (r *record) String() string {
-	if len(r.parts) == 1 {
-		return r.parts[0]
-	}
-
-	content := strings.Join(r.parts, " | ")
-	for s, r := range escapings {
-		content = strings.ReplaceAll(content, s, r)
-	}
-
-	return fmt.Sprintf("{%s}", content)
 }
